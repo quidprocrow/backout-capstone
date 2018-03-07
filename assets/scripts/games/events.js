f61efa0dac566f60dcc4ee889f9107e08bd0f1a1
quidprocrow <windmillwarrior@gmail.com>
2018-03-07 09:46:58 -0500

    Add function to load a html when new game button clicked
    Add function to populate the html of the new game area.
    Next will write the form.

--- assets/scripts/games/events.js
+++ assets/scripts/games/events.js
@@ -4,12 +4,21 @@ const fillers = require('../fillers.js')
 // const ui = require('./ui.js')
 // const store = require('../store.js')
 
+const gameButtonClick = function () {
+  const newGameHtml = (`
+    sup
+    `)
+  $('#new-game-form').html(newGameHtml)
+}
+
 const gameLinkClick = function () {
   fillers.showManyGames()
 }
 
 const addGamesEventListeners = function () {
   $('#game-link').on('click', gameLinkClick)
+  $('#new-game-button').on('click', gameButtonClick)
+
 }
 
 module.exports = {
